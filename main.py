2a0aa27960b8558d31532e9d9d97eb8d569fade3
Niump <43611165+Niump@users.noreply.github.com>
2020-01-03 19:43:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
 from discord.ext.commands import Bot
-from os import walk
-from os.path import join, splitext
+from os import walk, name
+from os.path import join, splitext, abspath, split
 from json import load
 
 
 class NPC(Bot):
     def __init__(self, *args, **kwargs) -> None:
+
         with open("database.json", encoding="utf-8") as file:
             self.database = load(file)
+
+        self.log_channel_id = self.database["Channels"]["log"]
+        self.main_channel_id = self.database["Channels"]["main"]
 
         super().__init__(*args, **kwargs)
 
 
 bot = NPC(command_prefix='ch ', case_insensitive=True)
 
-def extensions(tree: str) -> list:
-    """
-    :param tree: str - tree of extensions.
-    :return: list - extensions list of tree.
-        Syntax:
-            [(path, file), (path, file), ...]
-    """
-    for item in walk(tree):
-        #item return: (actual_path, folders_in_path, files_in_path)
+if __name__ == "__main__":
+    for item in walk("extensions"):
         files = filter(lambda file: file.endswith(".py"), item[-1])
         for file in files:
-            yield (item[0], file)
-
-if __name__ == "__main__":
-    for extension in extensions("extensions"):
-        path = join(*extension)
-        try:
-            path, ext = splitext(path)
-            bot.load_extension(path.replace("\\", '.'))
-        except Exception as error:
-            print("{0.__class__.__name__}: {0}".format(error))
+            file_name, ext = splitext(file)
+            path = join("extensions", file_name)
+            try:
+                bot.load_extension(path.replace("\\", '.'))
+            except Exception as error:
+                print("{0.__class__.__name__}: {0}".format(error))
 
     bot.run("")
