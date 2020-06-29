6391df60ac88926a146a13d81c71d04c2e66f106
Caio Alexandre <contato.kaiyo@gmail.com>
2020-06-29 11:46:48 -0300

    Create logging system

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
 
+import logging
+import contextlib
 from os import walk, name, getenv
 from os.path import join, splitext, abspath, split
 from json import load
@@ -21,9 +23,35 @@ class NPC(Bot):
         self.suggestion_channel_id = self.database["Channels"]["suggestion"]
 
 
-bot = NPC(command_prefix='.', case_insensitive=True)
+@contextlib.contextmanager
+def setup_logging() -> None:
+    try:
+        # __enter__
+        datetime_format = r'%Y-%m-%d %H:%M:%S'
 
-if __name__ == "__main__":
+        logging.getLogger('discord').setLevel(logging.INFO)
+        logging.getLogger('discord.http').setLevel(logging.WARNING)
+
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+
+        handler = logging.FileHandler(filename='chlorine.log', encoding='utf-8', mode='w')
+        formatter = logging.Formatter('[{asctime}] [{levelname:}] {name}: {message}', datetime_format, style='{')
+    
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        yield
+    finally:
+        # __exit__
+        for handler in logger.handlers:
+            handler.close()
+            logger.removeHandler(handler)
+
+
+def main():
+    bot = NPC(command_prefix='.', case_insensitive=True)
+
     for item in walk("extensions"):
         files = filter(lambda file: file.endswith(".py"), item[-1])
 
@@ -37,3 +65,7 @@ if __name__ == "__main__":
                 print("{0.__class__.__name__}: {0}".format(error))
 
     bot.run(TOKEN)
+
+if __name__ == "__main__":
+    with setup_logging():
+        main()
