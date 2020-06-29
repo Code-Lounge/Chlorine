6bdb58e51faaad05e85258878aa5a40f9e13e7f7
Niump <43611165+Niump@users.noreply.github.com>
2020-06-29 17:11:44 -0300

    PEP-8

--- extensions/moderation.py
+++ extensions/moderation.py
@@ -1,4 +1,4 @@
-from discord.ext.commands import Cog, Bot, command, Context, has_permissions, bot_has_permissions, guild_only
+from discord.ext.commands import Cog, command, has_permissions, bot_has_permissions, guild_only
 from discord import Member, Guild, Role
 from discord.errors import HTTPException
 from discord.utils import get
@@ -8,22 +8,21 @@ from os.path import basename
 
 
 class Moderation(Cog):
-    def __init__(self, bot: Bot) -> None:
+    def __init__(self, bot):
         self.bot = bot
 
     @guild_only()
     @has_permissions(ban_members=True)
     @bot_has_permissions(ban_members=True)
-    @command(name="ban", aliases=["banir",])
-    async def ban_command(self, ctx: Context, target: Member, reason: str="..."):
+    @command(name="ban", aliases=["banir", ])
+    async def ban_command(self, ctx, target: Member, reason="..."):
         """
-        Bane um membro do servidor.
-        É possível passar o motivo do banimento (fica salvo no registro de auditoria).
+        Bane um membro do servidor. É possível passar o motivo do banimento.
         """
         try:
             await ctx.guild.ban(target, reason=reason, delete_message_days=0)
         except HTTPException:
-            #A pessoa pode estar em uma posição hierarquica maior do que a do bot.
+            # `target` pode estar hierarquicamente maior que o bot.
             await ctx.send(f"Não foi possível banir {target.mention}.")
         else:
             await ctx.send(f"{target.mention} foi banido do servidor.")
@@ -31,16 +30,15 @@ class Moderation(Cog):
     @guild_only()
     @has_permissions(kick_members=True)
     @bot_has_permissions(kick_members=True)
-    @command(name="kick", aliases=["expulsar",])
-    async def kick_command(self, ctx: Context, target: Member, reason: str="..."):
+    @command(name="kick", aliases=["expulsar", ])
+    async def kick_command(self, ctx, target: Member, reason="..."):
         """
-        Expulsa um membro do servidor.
-        É possível passar o motivo da expulsão (fica salvo no registro de auditoria).
+        Expulsa um membro do servidor. É possível passar o motivo da expulsão.
         """
         try:
             await ctx.guild.kick(target, reason=reason, delete_message_days=0)
         except HTTPException:
-            #A pessoa pode estar em uma posição hierarquica maior do que a do bot.
+            # `target` pode estar hierarquicamente maior que o bot.
             await ctx.send(f"Não foi possível expulsar {target.mention}.")
         else:
             await ctx.send(f"{target.mention} foi expulso do servidor.")
@@ -48,12 +46,12 @@ class Moderation(Cog):
     @guild_only()
     @has_permissions(manage_roles=True)
     @bot_has_permissions(manage_roles=True)
-    @command(name="mute", aliases=["mutar",])
-    async def mute_command(self, ctx: Context, target: Member, reason: str="...", timeout: int=1):
+    @command(name="mute", aliases=["mutar", ])
+    async def mute_command(self, ctx, target: Member, reason="...", timeout: int=1):
         """
         Adiciona o cargo "Mutado" à um membro do servidor.
-        É possivel adicionar um motivo e o tempo em horas que o membro ficará mutado.
-        O servidor precisa ter um cargo chamado "Mutado" para este comando funcionar.
+        Pode-se adicionar um motivo e tempo em que o membro ficará mutado.
+        O servidor precisa ter um cargo chamado "Mutado" para este comando.
         """
         mute_role = get(ctx.guild.text_channels, name="Mutado")
 
@@ -61,13 +59,16 @@ class Moderation(Cog):
             try:
                 await target.add_roles(mute_role, reason=reason)
             except HTTPException:
-                await ctx.send(f"Não foi possível adicionar o cargo de mute para {target.mention}.")
+                await ctx.send(f"Não foi possível mutar {target.mention}.")
             else:
-                await ctx.send(f"{target.mention} foi mutado por {timeout} hora{'s' if timeout != 1 else ''}!")
+                message = "{0.mention} foi mutado por {1} hora{2}!".format(
+                    target, timeout, 's' if timeout != 1 else ''
+                )
+                await ctx.send(message)
                 await sleep(60 * 60 * timeout)
                 await target.remove_roles(self.mute_role)
         else:
-            await ctx.send("O servidor não possuí um cargo chamado `Mutado` para o uso deste comando.")
+            await ctx.send("O servidor precisa ter um cargo chamado `Mutado`.")
 
     @guild_only()
     @command(name="role")
@@ -83,10 +84,11 @@ class Moderation(Cog):
         except HTTPException:
             await ctx.send(f"Não foi possível adicionar o cargo {role.name} para {target.mention}.")
         else:
-            await ctx.send(f"O cargo {role.name} foi adicionado à {target.mention}.")
+            message = f"O cargo {role.name} foi adicionado à {target.mention}."
+            await ctx.send(message)
 
-def setup(bot: Bot):
-    # ~On load
+
+def setup(bot):
     try:
         bot.add_cog(Moderation(bot))
     except Exception as error:
@@ -94,6 +96,6 @@ def setup(bot: Bot):
     else:
         print(f"[{basename(__file__).upper()}] has been loaded.")
 
-def teardown(bot: Bot):
-    # ~On unload
+
+def teardown(bot):
     print(f"[{basename(__file__).upper()}] has been unloaded.")
